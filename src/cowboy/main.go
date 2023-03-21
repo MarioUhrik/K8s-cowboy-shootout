dfebee74e03978661c5e7bad707947b07fc725e7
MarioUhrik <mario.uhrik@berops.com>
2023-03-21 18:50:28 +0100

    Attempt to fix the segmentation fault

--- src/cowboy/main.go
+++ src/cowboy/main.go
@@ -37,7 +37,7 @@ func (s *server) GetShot(ctx context.Context, shooter *pb.Shooter) (*pb.Shooter,
 	cowboy.health = cowboy.health - shooter.Damage
 	log.Printf("%s has %d health left", cowboy.name, cowboy.health)
 	if (cowboy.health <= 0) {
-		defer die() // This causes a segmentation fault, possibly because we're shutting down the server while it's answering requests
+		die() // This may cause a segmentation fault, possibly because we're shutting down the server while it's answering requests
 	}
 
 	return shooter, nil
@@ -56,7 +56,7 @@ func (s *server) GetDeclaredVictorious(ctx context.Context, empty *pb.Empty) (*p
 
 func die() {
 	log.Printf("%s is dead", cowboy.name)
-	defer s.GracefulStop()
+	s.Stop()
 }
 
 func shoot() {

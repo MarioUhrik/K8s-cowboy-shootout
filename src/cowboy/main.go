dd9e3c236cdad3c9806996491cc10c49f9ff42ed
MarioUhrik <mario.uhrik@berops.com>
2023-04-04 17:39:24 +0200

    Refactor into an OOP style
    Stop using global variables

--- src/cowboy/main.go
+++ src/cowboy/main.go
@@ -18,53 +18,50 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-type server struct {
-	pb.UnimplementedCowboyServer
-}
-
 type Cowboy struct {
+	pb.UnimplementedCowboyServer
+
 	name   string
 	health int32
 	damage int32
+
+	cowboyServer    *grpc.Server
+	healthServer    *health.Server
+	triggerShutdown chan string
+
+	k8sConfig    *rest.Config
+	k8sClientset *kubernetes.Clientset
+	namespace    string
 }
 
-var cowboy Cowboy
-var s grpc.Server
-var healthServer *health.Server
-var triggerShutdown chan string
-
-var k8sConfig *rest.Config
-var k8sClientset *kubernetes.Clientset
-var namespace string
-
-func (s *server) GetShot(ctx context.Context, request *pb.GetShotRequest) (*pb.GetShotResponse, error) {
-	if cowboy.name == request.ShooterName {
-		log.Printf("%s didn't hit anyone", cowboy.name)
-		return &pb.GetShotResponse{VictimName: cowboy.name, RemainingHealth: cowboy.health}, nil
+func (self *Cowboy) GetShot(ctx context.Context, request *pb.GetShotRequest) (*pb.GetShotResponse, error) {
+	if self.name == request.ShooterName {
+		log.Printf("%s didn't hit anyone", self.name)
+		return &pb.GetShotResponse{VictimName: self.name, RemainingHealth: self.health}, nil
 	}
 
-	log.Printf("%s got shot by %s", cowboy.name, request.ShooterName)
-	cowboy.health = cowboy.health - request.IncomingDamage
-	log.Printf("%s has %d health left", cowboy.name, cowboy.health)
-	if cowboy.health <= 0 {
-		die()
+	log.Printf("%s got shot by %s", self.name, request.ShooterName)
+	self.health = self.health - request.IncomingDamage
+	log.Printf("%s has %d health left", self.name, self.health)
+	if self.health <= 0 {
+		self.die()
 	}
 
-	return &pb.GetShotResponse{VictimName: cowboy.name, RemainingHealth: cowboy.health}, nil
+	return &pb.GetShotResponse{VictimName: self.name, RemainingHealth: self.health}, nil
 }
 
-func listPods() *v1.PodList {
+func (self *Cowboy) listPods() *v1.PodList {
 	listOptions := meta_v1.ListOptions{LabelSelector: "microservice=cowboy"}
-	podList, err := k8sClientset.CoreV1().Pods(namespace).List(context.TODO(), listOptions)
+	podList, err := self.k8sClientset.CoreV1().Pods(self.namespace).List(context.TODO(), listOptions)
 	if err != nil {
 		log.Panicf("Failed to list the cowboy pods: %v", err)
 	}
 	return podList
 }
 
-func getRemainingCowboyIPs() []string {
+func (self *Cowboy) getRemainingCowboyIPs() []string {
 	var podIPs []string
-	for _, pod := range listPods().Items {
+	for _, pod := range self.listPods().Items {
 		if pod.Status.ContainerStatuses[0].Ready {
 			podIPs = append(podIPs, pod.Status.PodIP)
 		}
@@ -72,43 +69,45 @@ func getRemainingCowboyIPs() []string {
 	return podIPs
 }
 
-func isVictorious() bool {
-	return cowboy.health > 0 && len(getRemainingCowboyIPs()) == 1
+func (self *Cowboy) isVictorious() bool {
+	return self.health > 0 && len(self.getRemainingCowboyIPs()) == 1
 }
 
-func die() {
-	log.Printf("%s is dead", cowboy.name)
-	healthServer.Shutdown()
+func (self *Cowboy) die() {
+	log.Printf("%s is dead", self.name)
+	self.healthServer.Shutdown()
 	time.Sleep(1 * time.Second) // avoid io timeout error on connections from other cowboys at this time
-	triggerShutdown <- "Shutting down the GRPC server"
+	self.triggerShutdown <- "Shutting down the GRPC server"
 }
 
-func shoot() {
-	conn, err := grpc.Dial("cowboys:8080", grpc.WithTimeout(100*time.Millisecond), grpc.WithInsecure())
+func (self *Cowboy) shoot() {
+	ctx, _ := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	conn, err := grpc.DialContext(ctx, "cowboys:8080", grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Printf("Failed to Dial cowboy while shooting: %v", err)
 		return
 	}
 	client := pb.NewCowboyClient(conn)
-	log.Printf("%s shoots", cowboy.name)
-	_, err = client.GetShot(context.Background(), &pb.GetShotRequest{ShooterName: cowboy.name, IncomingDamage: cowboy.damage})
+	log.Printf("%s shoots", self.name)
+	_, err = client.GetShot(ctx, &pb.GetShotRequest{ShooterName: self.name, IncomingDamage: self.damage})
 	if err != nil {
 		log.Printf("Failed to hit target cowboy while shooting: %v", err)
 	}
 	conn.Close()
 }
 
-func getReady() {
-	k8sConfig, err := rest.InClusterConfig()
+func (self *Cowboy) getReady() {
+	var err error
+	self.k8sConfig, err = rest.InClusterConfig()
 	if err != nil {
 		log.Panicf("Failed to load the InClusterConfig for Kubernetes: %v", err)
 	}
-	k8sConfig.Timeout = time.Second
-	k8sClientset, err = kubernetes.NewForConfig(k8sConfig)
+	self.k8sConfig.Timeout = time.Second
+	self.k8sClientset, err = kubernetes.NewForConfig(self.k8sConfig)
 	if err != nil {
 		log.Panicf("Failed to use the InClusterConfig for Kubernetes: %v", err)
 	}
-	namespace = os.Getenv("K8S_NAMESPACE")
+	self.namespace = os.Getenv("K8S_NAMESPACE")
 
 	cowboyHealth, err := strconv.Atoi(os.Getenv("COWBOY_HEALTH"))
 	if err != nil {
@@ -119,55 +118,56 @@ func getReady() {
 		log.Panicf("Failed to parse COWBOY_DAMAGE env variable: %v", err)
 	}
 
-	cowboy.name = os.Getenv("COWBOY_NAME")
-	cowboy.health = int32(cowboyHealth)
-	cowboy.damage = int32(cowboyDamage)
+	self.name = os.Getenv("COWBOY_NAME")
+	self.health = int32(cowboyHealth)
+	self.damage = int32(cowboyDamage)
 
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Panicf("Failed to listen on TCP port: %v", err)
 	}
 
-	triggerShutdown = make(chan string)
-	s := grpc.NewServer()
+	self.triggerShutdown = make(chan string)
+	self.cowboyServer = grpc.NewServer()
 	go func() {
-		pb.RegisterCowboyServer(s, &server{})
-		healthServer = health.NewServer()
-		healthgrpc.RegisterHealthServer(s, healthServer)
-		if err := s.Serve(listener); err != nil {
+		pb.RegisterCowboyServer(self.cowboyServer, self)
+		self.healthServer = health.NewServer()
+		healthgrpc.RegisterHealthServer(self.cowboyServer, self.healthServer)
+		if err := self.cowboyServer.Serve(listener); err != nil {
 			log.Panicf("Failed to serve: %v", err)
 		}
-		log.Printf("%s is now ready", cowboy.name)
+		log.Printf("%s is now ready", self.name)
 
-		<-triggerShutdown
-		s.GracefulStop()
+		<-self.triggerShutdown
+		self.cowboyServer.GracefulStop()
 		listener.Close()
 	}()
 }
 
-func waitForReadiness() {
+func (self *Cowboy) waitForReadiness() {
 	ready := false
 	for !ready {
 		time.Sleep(1 * time.Second)
-		if len(getRemainingCowboyIPs()) == len(listPods().Items) {
+		if len(self.getRemainingCowboyIPs()) == len(self.listPods().Items) {
 			ready = true
 		}
 	}
 }
 
-func shootout() {
-	for cowboy.health > 0 {
-		shoot()
+func (self *Cowboy) shootout() {
+	for self.health > 0 {
+		self.shoot()
 		time.Sleep(1000 * time.Millisecond)
-		if isVictorious() {
-			log.Printf("%s is victorious! The fastest hand in the West.", cowboy.name)
+		if self.isVictorious() {
+			log.Printf("%s is victorious! The fastest hand in the West.", self.name)
 			time.Sleep(3600 * time.Second)
 		}
 	}
 }
 
 func main() {
-	getReady()
-	waitForReadiness()
-	shootout()
+	cowboy := Cowboy{}
+	cowboy.getReady()
+	cowboy.waitForReadiness()
+	cowboy.shootout()
 }
